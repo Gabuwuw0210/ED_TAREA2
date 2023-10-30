60729a8ab4c9b481599f29d4d8e09ca46fa4c5f5
Gabu <97574076+Gabuwuw0210@users.noreply.github.com>
2023-10-29 22:46:19 -0300

    test.cpp

---
+++ test.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <fstream>
+#include "DeBlock.hpp"
+
+int main() {
+    std::ifstream inputFile("pruebas.txt");
+    if (!inputFile.is_open()) {
+        std::cerr << "Error al abrir el archivo de entrada." << std::endl;
+        return 1;
+    }
+
+    int n, b;
+    inputFile >> n >> b;
+    int* elems = new int[n];
+    for (int i = 0; i < n; i++) {
+        inputFile >> elems[i];
+    }
+
+    DeBlock deBlock(elems, n, b);
+    std::cout << "blockSize: " << deBlock.getBlockSize() << std::endl;
+    std::cout << "cant_elems: " << deBlock.getCantElems() << std::endl;
+
+    int q;
+    inputFile >> q;
+
+    for (int i = 0; i < q; i++) {
+        char op;
+        inputFile >> op;
+
+        if (op == 'I') {
+            int pos, val;
+            inputFile >> pos >> val;
+            int result = deBlock.insert(pos, val);
+            std::cout << result << std::endl;
+        }
+        else if (op == 'G') {
+            int pos;
+            inputFile >> pos;
+            int value = deBlock.get_value(pos);
+            std::cout << value << std::endl;
+        }
+        else if (op == 'L') {
+            int len = deBlock.length();
+            std::cout << len << std::endl;
+        }
+    }
+
+    delete[] elems;
+    return 0;
+}
